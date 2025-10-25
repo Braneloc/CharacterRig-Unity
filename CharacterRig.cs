50c7e894b6dd9b2d93997688c8324033aff09c67
Braneloc <Braneloc@users.noreply.github.com>
2025-10-25 17:09:25 +0100

    Initial commit

---
+++ CharacterRig.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace Walkabout.Player
+{
+    [DisallowMultipleComponent]
+    public class CharacterRig : MonoBehaviour, ICharacterRig
+    {
+        [SerializeField] Transform head;
+        [SerializeField] Transform viewPivot;
+        public Transform Body => transform;
+        public Transform Head => head;
+        public Transform ViewPivot => viewPivot;
+
+        void OnValidate()
+        {
+            if (head == null)
+                head = transform.Find("Head");
+            if (head == null)
+                Debug.LogError("No head found automatically as child of Agent", this);
+            if (viewPivot == null)
+                viewPivot = head;
+        }
+    }
+    /// <summary>
+    /// Represents an character with body, head, and eye line transforms.
+    /// </summary>
+    /// <remarks>The ICharacterRig interface provides access to key transforms that define the agent's physical
+    /// representation and orientation in a scene. Implementations may use these transforms for positioning, animation,
+    /// or interaction logic.
+    /// 
+    /// If you are using a CharacterController component, position your mesh
+    /// such that the feet are at (height/2) - skinwidth or it will float.
+    /// </remarks>
+    public interface ICharacterRig
+    {
+        Transform Body { get; }
+        Transform ViewPivot { get; }
+        Transform Head { get; }
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Character Rig
+
+## Overview
+
+Contains a super simple rig for a character in a standard format.
+The 3D model is included, and is deliberatly broken into three
+parts (head, body, legs) to allow the head/legs to be moved separately
+from the body - for  example, to allow the character to look around.
+
+It is also intended as a starting point for building more complex rigs,
+and to be controlled by a separate control system.
